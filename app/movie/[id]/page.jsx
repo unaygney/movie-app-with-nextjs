388bf960e55d25d5346bea174acdf42bffd742e0
Guney Unay <guneymarketing17@hotmail.com>
2023-11-11 18:22:33 +0300

    added not found page

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MovieContainer from "@/containers/movie";
 import Movies from "@/mocks/movies.json";
+import { notFound } from "next/navigation";
 
 const delay = (ms) => {
   return new Promise((resolve) => {
@@ -9,8 +10,13 @@ const delay = (ms) => {
 };
 
 async function MoviePage({ params }) {
-  await delay(1000)
+  await delay(1000);
+
   const movie = Movies.results.find((e) => e.id === +params.id);
+
+  if (!movie) {
+    notFound();
+  }
   return <MovieContainer movie={movie} />;
 }
 
