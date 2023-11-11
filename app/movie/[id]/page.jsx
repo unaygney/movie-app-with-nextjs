d8df2d910562311554fc211faa8eef99ee169708
Guney Unay <guneymarketing17@hotmail.com>
2023-11-11 18:15:15 +0300

    added loading page

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import MovieContainer from "@/containers/movie";
 import Movies from "@/mocks/movies.json";
 
-function MoviePage({ params }) {
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+async function MoviePage({ params }) {
+  await delay(1000)
   const movie = Movies.results.find((e) => e.id === +params.id);
   return <MovieContainer movie={movie} />;
 }
